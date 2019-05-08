bd936717a25f2bc3ccdc0793caf9187489817034
Noah-Amar <ne.amar@outlook.com>
2019-05-07 18:31:09 -0600

    Update bench_geokdbush.py

--- bench/bench_geokdbush.py
+++ bench/bench_geokdbush.py
@@ -1,1 +1,14 @@
 
+
+import sys
+sys.path.insert(0, '../')
+cities = import all_the_cities
+kdbush = import kdbush
+geokdbush = import ../
+
+print("=== geokdbush benchmark ===")
+
+n = cities.length
+k = 1000
+
+random = []
